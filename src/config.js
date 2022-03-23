1692fc53fcddd833f2fde56dd56610b03e11ff91
JuanPabloLino <78151174+JuanPabloLino@users.noreply.github.com>
2022-03-23 10:13:21 +0100

    Subida de Archivos 23/3

---
+++ src/config.js
@@ -0,0 +1,23 @@
+export default {
+  fileSystem: {
+    path: "../DB",
+  },
+  mongodb: {
+    cnxStr: "mongodb://127.0.0.1:27017/ecommerce",
+    options: {
+      serverSelectionTimeoutMS: 5000,
+    },
+  },
+  firebase: {
+  type:"service_account",
+  project_id: "backendjuanlino",
+  private_key_id: "496011f894046049aab0bd63cc2144e636207be1",
+  private_key: "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCetia27ZeePzX9\n8w1f5aWBHL/BIqparVQwNFU6tcz4iZP41XYgJdfdfmBWW3LRntEdZrq+S88/ZNa0\nhrMmPEpE17OyhjoLgBlqbWABMc6D8C4DjLfOWRjigOUJpm9bYK0S3atYyOFCcCgP\nAsZO/vWf9Tmw9FEo9x0sUIgCvFz1BJdJAeY7mPJPnZcGJ7bAyPEd0lvD0rohxtlh\nnlj6PoPRMBo4Vc3I1JAox6ZUBXtWXlxHwJl9x34JCJvzALCRxT40KkNUdYaT7irV\n6N5djs9VXRyvOe2IUQOOhu1+cqlAwPdh7QAnVy5tklZvQBzSUN3KLpQ8/hn9yRdu\n3gjgSdm/AgMBAAECggEAEDBqYIx5w8WU31OUkKyfJj1lffDoSZmWldgwvW1f/BYk\nhuRXSU4y58AFWam4W5OEpygccl7PRBsoH4AB9CuAM+eAx/wTHasybH29pkGLXD/6\noZgQA7do52nYcxSMFVkWLm94E/VhrOI949IXomU1btyXxuoVAUW7rxKD3SuTm9Zz\nvJaDCPtbo38JtfvJesg7v3Ffuvrv0c6YDFH3DmHX2HxWTEHCj7jgY7AqSE88SiXs\nwAky3YGzTQTGRdEGxrZMafiA6n7Bkw/lJytEx0Aa15qqv/Fv2110jgVX9sU1wACZ\nhmPr96vrFY+zSLShmC+imOEjJcvyjKpOGuCOLMMuqQKBgQDd0BgwR4PeIdGkPtnh\n+EYS5wCiagRaKCLACufxHETc8GD8wKuDth+hVtvfoPrYulgOMkcR/d96JP4/yBvu\nU7CX8BQKQulzOdYUGJKYRyHs8c7Av6BCZ6eaCTg9AC9pGAupn6iLMmSoZPqnZy89\n+WGG9vkhYl35z3q1ArepTeZBhQKBgQC3LFBkrFdhp5YPTqgcwWY92S8ePmZfyyKr\nIUCsZrq5l3e8I0y+DJnkf0KDx8cFqcOjSoPLylRbthV+HMBbKcgX89zS0l+c4Afr\nlDsMW409Y2InS7EcyzbgN04TyXeAwQf2N7M6zYIZhr5UOCEE9SY5R7YeJg/4oyYb\ndJZ1u3cvcwKBgQCWFm2XlXxz/Nw2z9RsY6s+tRIn06a4PnotCfQhoKgz+28U92XW\nzJAggn5qlinGJmjYykpusSLbKlbjX4sOu5A9btSQkPAX6KWjmXDwGwmoo4HryH4v\nEKZGw0BLfmmWKhASKZdJAMM9cn5scZR00d6dDVZLsZy3IR39hyYxKCXKZQKBgD0n\nVk/GGgZIZRkHMakoCHJtqAV438TrlM99cpIcWgrMPvVY864fluFqe1JzyEZjvyUt\nI4NOMYvM5rAL+Ux2To9wozDfe3xsbzh/bJdbmXP9Ui+8XY03a0LANBj3EiayS24w\nem4PXngXPEPx2VRGZFSLVIoMOxjPn1FbQ0BbLhdxAoGBAJCNAjds5asVZ/sojJTH\njTPaGRRTUHezlHhIq4LzavzUAD7UmYCxzpqR0L7B6oJw0q5enpJwRDe9oJmFbhw4\n/J+Jsi1TdxeH0pDi8TpwrsJlv/32hTUdqe7D1qUlN5WmcO8ViWH43EzgH8gz5QSP\nN7NuU+VXVYhgTn1MVwsWLH0l\n-----END PRIVATE KEY-----\n",
+  client_email: "firebase-adminsdk-agvgs@backendjuanlino.iam.gserviceaccount.com",
+  client_id: "110332094954619828263",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-agvgs%40backendjuanlino.iam.gserviceaccount.com"
+},
+};
